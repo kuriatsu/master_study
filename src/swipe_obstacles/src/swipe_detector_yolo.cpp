ca68862cf49b3855cfda542971bb1055b97a1ee7
kuriatsu <kuribayashi.atsushi@g.sp.m.is.nagoya-u.ac.jp>
2019-05-29 20:35:01 +0900

    publish obstacle frame_id changed to original

--- src/swipe_obstacles/src/swipe_detector_yolo.cpp
+++ src/swipe_obstacles/src/swipe_detector_yolo.cpp
@@ -1,7 +1,6 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Pose.h>
 #include <sensor_msgs/CameraInfo.h>
-#include <tf/transform_listener.h>
 
 // #include <opencv2/opencv.hpp>
 
@@ -20,7 +19,6 @@ class SwipeDetectorYolo{
         double lidar_hight;
         double camera_mat[9] = {0.0};
         std::string camera_frame_id;
-        tf::TransformListener tf_listener;
 
 
     public:
@@ -30,47 +28,54 @@ class SwipeDetectorYolo{
         void sub_obj_callback(const autoware_msgs::DetectedObjectArray &msgs);
         void sub_camera_info_callback(const sensor_msgs::CameraInfo &msgs);
         swipe_obstacles::detected_obstacle calc_obj_info(const autoware_msgs::DetectedObject &in_obj);
-        geometry_msgs::Pose tf_transformer(const geometry_msgs::Pose &in_pose);
 };
 
-SwipeDetectorYolo::SwipeDetectorYolo(double in) : lidar_hight(in){
 
+SwipeDetectorYolo::SwipeDetectorYolo(double in) : lidar_hight(in)
+{
     ros::NodeHandle n;
 
     pub_obstacle_pose = n.advertise<swipe_obstacles::detected_obstacle_array>("/detected_obstacles", 5);
     sub_obj = n.subscribe("/detection/vision_objects", 5, &SwipeDetectorYolo::sub_obj_callback, this);
     sub_camera_info = n.subscribe("/camera_info", 5, &SwipeDetectorYolo::sub_camera_info_callback, this);
-
-
 }
 
-void SwipeDetectorYolo::sub_obj_callback(const autoware_msgs::DetectedObjectArray &msgs){
 
+// suscribe object positions array in 2D image
+// publish objects array in 3D world. frame_id is same as input obstacles.
+void SwipeDetectorYolo::sub_obj_callback(const autoware_msgs::DetectedObjectArray &msgs)
+{
     swipe_obstacles::detected_obstacle_array out_obstacle_array;
-    ROS_INFO("Get obj info");
 
-    for(size_t i=0; i<msgs.objects.size(); i++){
+    // calc each obstacle position and put them into container
+    for(size_t i=0; i<msgs.objects.size(); i++)
+    {
         out_obstacle_array.obstacles.push_back(calc_obj_info(msgs.objects[i]));
     }
 
-    out_obstacle_array.header.frame_id = "world";
+    // add adititonal informations
+    out_obstacle_array.header.frame_id = camera_frame_id;
     out_obstacle_array.header.stamp = ros::Time::now();
+
+    // publish
     pub_obstacle_pose.publish(out_obstacle_array);
 }
 
 
-swipe_obstacles::detected_obstacle SwipeDetectorYolo::calc_obj_info(const autoware_msgs::DetectedObject &in_obj){
-
+// input: yolo rectangle position in 2D image
+// output: 3D position
+swipe_obstacles::detected_obstacle SwipeDetectorYolo::calc_obj_info(const autoware_msgs::DetectedObject &in_obj)
+{
     swipe_obstacles::detected_obstacle out_pose;
-    geometry_msgs::Pose camera_obj_pose;
 
-    // camera_mat = cv::Mat(3, 3, )
+    // calc obstacle position using camera intrinsic parametors
+    out_pose.pose.position.y = lidar_hight;
+    out_pose.pose.position.z = lidar_hight * camera_mat[4] / ((in_obj.y + in_obj.height) - camera_mat[5]);
+    out_pose.pose.position.x = (((in_obj.x + in_obj.width / 2) - camera_mat[2]) * out_pose.pose.position.z) / camera_mat[0];
 
-    camera_obj_pose.position.y = lidar_hight;
-    camera_obj_pose.position.z = lidar_hight * camera_mat[4] / ((in_obj.y + in_obj.height) - camera_mat[5]);
-    camera_obj_pose.position.x = (((in_obj.x + in_obj.width / 2) - camera_mat[2]) * camera_obj_pose.position.z) / camera_mat[0];
+    ROS_INFO_STREAM(out_pose.pose.position.z);
 
-    out_pose.pose = tf_transformer(camera_obj_pose);
+    // add adtional informations
     out_pose.id = in_obj.id;
     out_pose.label = in_obj.label;
     out_pose.score = in_obj.score;
@@ -79,28 +84,8 @@ swipe_obstacles::detected_obstacle SwipeDetectorYolo::calc_obj_info(const autowa
 }
 
 
-geometry_msgs::Pose SwipeDetectorYolo::tf_transformer(const geometry_msgs::Pose &in_pose)
-{
-    geometry_msgs::Pose world_pose;
-    tf::Pose world_to_camera;
-    tf::Pose req_to_camera;
-    tf::StampedTransform req_to_world;
-
-    try{
-		tf_listener.waitForTransform(camera_frame_id, "world", ros::Time(0), ros::Duration(1.0));
-		tf_listener.lookupTransform("world", camera_frame_id, ros::Time(0), req_to_world);
-
-	}catch(...){
-		ROS_INFO("velodyne to world transform ERROR");
-	}
-
-	tf::poseMsgToTF(in_pose, world_to_camera);
-	req_to_camera = req_to_world * world_to_camera;
-	tf::poseTFToMsg(req_to_camera, world_pose);
-
-    return world_pose;
-}
-
+// input: camera infor
+// save informations to class valiables
 void SwipeDetectorYolo::sub_camera_info_callback(const sensor_msgs::CameraInfo &msgs)
 {
     if(camera_mat[0]==0.0)
@@ -112,12 +97,11 @@ void SwipeDetectorYolo::sub_camera_info_callback(const sensor_msgs::CameraInfo &
         {
             camera_mat[i] = msgs.K[i];
         }
-        ROS_INFO_STREAM(camera_frame_id);
     }
 }
 
-int main(int argc, char **argv){
-
+int main(int argc, char **argv)
+{
     ros::init(argc, argv, "swipe_detector_yolo_node");
 
     ROS_INFO("Initialized");
