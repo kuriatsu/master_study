00f9997c55ad3cf045dc4a47f7572a1f452d644e
kuriatsu <kuribayashi.atsushi@g.sp.m.is.nagoya-u.ac.jp>
2020-01-17 18:52:31 +0900

    testing controler

---
+++ src/carla_helper/script/test.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import glob
+import os
+import sys
+
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+import carla
+
+import argparse
+import logging
+import time
+import random
+
+def main():
+
+	client = carla.Client("127.0.0.1", 2000)
+	client.set_timeout(2.0)
+	world = client.get_world()
+
+	# get ego car
+	for carla_actor in world.get_actors():
+		if carla_actor.type_id.startswith("vehicle"):
+			if carla_actor.attributes.get('role_name') == 'ego_vehicle':
+				ego_vehicle = carla_actor
+		
+	batch = []
+	batch.append(carla.command.ApplyVelocity(ego_vehicle, carla.Vector3D(100.0,0.0,0.0)))
+	client.apply_batch(batch)
+	print("accel")
+	print(ego_vehicle.get_velocity())
+	time.sleep(0.02)
+	print(ego_vehicle.get_velocity())
+	time.sleep(0.02)
+	print(ego_vehicle.get_velocity())
+	time.sleep(0.02)
+	print(ego_vehicle.get_velocity())
+	time.sleep(0.02)
+	print(ego_vehicle.get_velocity())
+	time.sleep(0.02)
+	print(ego_vehicle.get_velocity())
+	time.sleep(1.7)
+	batch.append(carla.command.ApplyVelocity(ego_vehicle, carla.Vector3D(0.0,0.0,0.0)))
+	client.apply_batch(batch)
+	print("brake")
+	print(ego_vehicle.get_velocity())
+	time.sleep(0.02)
+	print(ego_vehicle.get_velocity())
+	time.sleep(0.02)
+	print(ego_vehicle.get_velocity())
+	time.sleep(0.02)
+	print(ego_vehicle.get_velocity())
+	time.sleep(0.02)
+	print(ego_vehicle.get_velocity())
+	time.sleep(0.02)
+	print(ego_vehicle.get_velocity())
+
+	# while True:
+	# 	world.wait_for_tick()
+	# 	ego_pose = ego_vehicle.get_transform()
+	# 	time.sleep(0.1)
+
+
+if __name__ == '__main__':
+
+	main()
