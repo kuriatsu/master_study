c4785704f687a4531fd86d2ba1e9305aba7b5df1
kuriatsu <kuribayashi.atsushi@g.sp.m.is.nagoya-u.ac.jp>
2020-02-24 12:34:01 +0000

    develop wall type hmi

--- src/ras_carla/src/ras_visualizer/ras_visualizer.cpp
+++ src/ras_carla/src/ras_visualizer/ras_visualizer.cpp
@@ -11,7 +11,8 @@ RasVisualizer::RasVisualizer(): marker_scale(1.0), marker_vertical_shrink_rate(0
     sub_obj = n.subscribe("/managed_objects", 5, &RasVisualizer::subObjCallback, this);
 	// sub_vehicle_info = n.subscribe("/ego_vehicle/", 5, &RasVisualizer::subVehicleInfoCallback, this);
 	// sub_erase_signal = n.subscribe("/erase_signal", 5, &RasVisualizer::erase_signal_callback, this);
-	pub_shift = n.advertise<ras_carla::RasObject>("/shifted_info", 5);
+    pub_shift = n.advertise<ras_carla::RasObject>("/control_info", 5);
+	pub_marker = n.advertise<visualization_msgs::MarkerArray>("/feedback_info", 5);
 }
 
 
@@ -23,23 +24,60 @@ RasVisualizer::~RasVisualizer()
 void RasVisualizer::subObjCallback(const ras_carla::RasObjectArray &in_obj_array)
 {
     server->clear();
+    visualization_msgs::MarkerArray marker_array;
     // ROS_INFO("visualezer subscribed");
-
-    for (size_t i=0; i< in_obj_array.objects.size(); i++)
+    for (auto itr : in_obj_array.objects)
     {
-        createInteractiveMarker(in_obj_array.objects[i]);
+        if (itr.is_interaction)
+        {
+            createInteractiveMarker(itr);
+        }
+        else
+        {
+            marker_array.emplace_back(createMarker(itr));
+        }
     }
-
     server->applyChanges();
+    pub_marker.publish(marker_array);
+}
+
+visualization_msgs::Marker RasVisualizer::createMarker(ras_carla::RasObject &in_obj)
+{
+    visualization_msgs::Marker marker;
+
+    marker.header = in_msgs.object.header;
+    marker.ns = "ras";
+    marker.id = in_obj.object.id;
+
+    marker.type = visualization_msgs::Marker::CUBE;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.pose = in_obj.object.pose;
+    marker.scale.x = marker_scale*in_obj.object.shape.dimensions[0];
+    marker.scale.y = marker_scale*in_obj.object.shape.dimensions[1];
+    marker.scale.z = marker_scale*in_obj.object.shape.dimensions[2];
+
+    if (marker.id == 8888)
+    {
+        marker.color.r = 1;
+        marker.color.g = 0;
+    }
+    else
+    {
+        marker.color.r = 0;
+        marker.color.g = 1;
+    }
+    marker.color.b = 0;
+    marker.color.a = 0.3;
+
+    marker.lifetime = ros::duration(0.1);
+    return marker;
 }
 
 
-void RasVisualizer::createInteractiveMarker(ras_carla::RasObject in_obj)
+void RasVisualizer::createInteractiveMarker(ras_carla::RasObject &in_obj)
 {
     // for debag
     // std::cout <<"ss id is:" << in_obj.id << std::endl;
-
-
     std::stringstream ss;
     ss << in_obj.object.id;
 
@@ -72,14 +110,9 @@ void RasVisualizer::setMarkerControl(visualization_msgs::InteractiveMarker &int_
 	visualization_msgs::InteractiveMarkerControl control;
 
 	control.always_visible  = true;
-	control.interaction_mode = visualization_msgs::InteractiveMarkerControl::MOVE_AXIS;
-	control.orientation.x = 0;
-	control.orientation.y = 0;
-	control.orientation.z = 1;
-	control.orientation.w = 1;
+	control.interaction_mode = visualization_msgs::InteractiveMarkerControl::BUTTON;
 
     setMarkerToMarkerControl(control, in_obj);
-	// control.markers.push_back(setMarkerToMarkerControl(in_obj));
     int_marker.controls.push_back(control);
 }
 
@@ -91,48 +124,14 @@ void RasVisualizer::setMarkerToMarkerControl(visualization_msgs::InteractiveMark
     marker.ns = "ras";
     marker.id = in_obj.object.id;
 
-    if (in_obj.object.classification == 4)
-    {
-        marker.type = visualization_msgs::Marker::CYLINDER;
-        marker.scale.x = marker_scale*in_obj.object.shape.dimensions[0];
-        marker.scale.y = marker_scale*in_obj.object.shape.dimensions[1];
-        marker.scale.z = marker_scale*in_obj.object.shape.dimensions[2] * marker_vertical_shrink_rate;
-    }
+    marker.type = visualization_msgs::Marker::CUBE;
+    marker.scale.x = marker_scale*in_obj.object.shape.dimensions[0];
+    marker.scale.y = marker_scale*in_obj.object.shape.dimensions[1];
+    marker.scale.z = marker_scale*in_obj.object.shape.dimensions[2] * marker_vertical_shrink_rate;
 
-    else if (in_obj.object.classification == 5)
-    {
-        marker.type = visualization_msgs::Marker::ARROW;
-        marker.scale.x = marker_scale*in_obj.object.shape.dimensions[0];
-        marker.scale.y = marker_scale*in_obj.object.shape.dimensions[1];
-        marker.scale.z = marker_scale*in_obj.object.shape.dimensions[2] * marker_vertical_shrink_rate;
-    }
-
-    else if (in_obj.object.classification == 6)
-    {
-        marker.type = visualization_msgs::Marker::CUBE;
-        marker.scale.x = marker_scale*in_obj.object.shape.dimensions[0];
-        marker.scale.y = marker_scale*in_obj.object.shape.dimensions[1];
-        marker.scale.z = marker_scale*in_obj.object.shape.dimensions[2] * marker_vertical_shrink_rate;
-    }
-
-    if(in_obj.importance == 1.0)
-    {
-        marker.color.r = 1;
-        marker.color.g = 0;
-        marker.color.b = 0;
-    }
-    else if(in_obj.importance == 0.5)
-    {
-        marker.color.r = 0;
-        marker.color.g = 1;
-        marker.color.b = 0;
-    }
-    else
-    {
-        marker.color.r = 0;
-        marker.color.g = 0;
-        marker.color.b = 1;
-    }
+    marker.color.r = 1;
+    marker.color.g = 0;
+    marker.color.b = 0;
     marker.color.a = 0.3;
 
     // marker.lifetime = ros::Duration(2.0);
@@ -145,8 +144,6 @@ void RasVisualizer::shiftFeedback(const visualization_msgs::InteractiveMarkerFee
     ras_carla::RasObject feedback_obj;
     std::istringstream sis;
 
-    sis = std::istringstream(feedback->marker_name);
-    feedback_obj.object.pose = feedback->pose;
-    sis >> feedback_obj.object.id;
+    std::istringstream(feedback->marker_name) >> feedback_obj.object.id;
     pub_shift.publish(feedback_obj);
 }
